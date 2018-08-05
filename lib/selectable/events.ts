7cfc99a584323fc89b80ecc242e6d9fdabf32215
Don <don.j.olmstead@gmail.com>
2018-08-04 18:47:55 -0700

    feat(select): Add selection events

---
+++ lib/selectable/events.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright (c) 2018 The Rei Project Authors. All rights reserved.
+ * Please see the AUTHORS file for details. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be found in
+ * the LICENSE file.
+ */
+
+/** The name for the selection event. */
+export const selectionEvent = 'selection-changed';
+
+/**
+ * Fires a selection changed event on the element.
+ * 
+ * @param element The element that was selected.
+ */
+export function fireSelectionEvent(element: Element): void {
+  element.dispatchEvent(new CustomEvent(selectionEvent, {
+    bubbles: true,
+    cancelable: true,
+    detail: element
+  }));
+}
