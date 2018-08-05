4ba82221e0c080ad3f08bbd7ee4de84b8fb9ccf9
Don <don.j.olmstead@gmail.com>
2018-08-05 15:36:04 -0700

    test

--- wct.conf.js
+++ wct.conf.js
@@ -1,11 +1,3 @@
 module.exports = {
   suites: ['test/index.html'],
-  plugins: {
-    istanbub: {
-      reporters: ["text-summary", "lcov"],
-      include: [
-        'lib/**/*.js'
-      ]
-    }
-  }
 };
